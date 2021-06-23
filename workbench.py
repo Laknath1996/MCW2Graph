fd2f9c15733a6bc6e125156e48d4d2e3e24a0d30
Laknath1996 <ashwindesilva@yahoo.com>
2021-06-23 21:59:50 +0530

    testing update

--- workbench.py
+++ workbench.py
@@ -28,85 +28,50 @@ import os
 import numpy as np
 import scipy.io as sio
 from sklearn.manifold import TSNE
+import pandas as pd
 
 # internal
 from graph_learning.methods import DiffusionGraphLearn, GraphicalLassoGraphLearn
-from graph_learning.utils import createWeightedGraph
-from data_handling.utils import create_dataset, plot_recording
+from graph_learning.utils import createWeightedGraph, plotMCWandGraph
+from data_handling.utils import create_dataset, plot_recording_same_axis, preprocess, read_from_csv
 from tma.utils import plot_latent_space
 
 #////// body //////
 
 ## User Inputs
-id = "kushaba"
-s_id = 1
-labels = ["M_M", "R_R", "I_I", "L_L", "HC_"] # "T_I", "T_M", "T_R", "T_L", "I_M", "M_R", "R_L", "IMR", "MRL"]
-visualizeLearntGraphs = True
-visualizeLatentSpace = True
-L = 8
+id = "de_silva"
+file = "data/de_silva/subject_1001_Ashwin/R_2.csv"
+onsets_file = "data/de_silva/subject_1001_Ashwin/onsets.csv"
+fs = 200
+path = "figures/subject_1001"
 
-## Select the Graph Learning Method
-useJustMCW = False
-useGraphicalLassoGraphLearn = False
-useDiffusionGraphLearn = True
+data = read_from_csv(id, file)
+data = preprocess(data, fs, lpfreq=1)
+# plot_recording_same_axis(data, fs)
 
-## Create dataset
-X, y = create_dataset(id, s_id, labels)
+df = pd.read_csv(onsets_file, delimiter=',')
+onsets = df[['R_2']].values.squeeze()
 
-## collect the learnt the W matrices
-Ws = np.empty((0, 8, 8))
+p = 5
+beta_1 = 10
+beta_2 = 0.1
 
-if useJustMCW:
-    Ws = X
-
-if useDiffusionGraphLearn:
-    # hyperparams
-    p = 5
-    beta_1 = 10
-    beta_2 = 0.1
-
-    for i in range(X.shape[0]):
-        GL = DiffusionGraphLearn(
-            1000*X[i],
+for i in range(len(onsets)):
+    start = int((onsets[i])*200)
+    end = int((onsets[i]+1)*200)
+    X = data[:, start:end]
+    
+    GL = DiffusionGraphLearn(
+            X,
             p=p, 
             beta_1=beta_1,
             beta_2=beta_2,
             verbosity=True
-        )
-        W = GL.findGraphLaplacian()
-        Ws = np.vstack((Ws, np.expand_dims(W, 0)))
-        if visualizeLearntGraphs:
-            createWeightedGraph(W, "Class : {}".format(int(y[i])))
-        print('Graph {:n} : Completed!'.format(i+1))
+    )
+    W = GL.findGraphLaplacian()
+    save_path = os.path.join(path, "R_1_{}.png".format(i))
+    plotMCWandGraph(X, fs, W, title="Ring Flexion", save_path=save_path)
 
-if useGraphicalLassoGraphLearn:
-    # hyperparams
-    beta = 0.5
-    gamma = 0.0001
-    imax = 2000
-    epsilon = 0.01
-    alpha = 0.05
 
-    for i in range(X.shape[0]):
-        GL = GraphicalLassoGraphLearn(
-            X=10000*X[i],
-            beta=beta,
-            gamma=gamma,
-            imax=imax,
-            epsilon=epsilon,
-            alpha=alpha,
-            verbosity=False
-        )
-        W = GL.findGraph()
-        if visualizeLearntGraphs:
-            createWeightedGraph(W, "Class : {}".format(labels[int(y[i])]))
-        Ws = np.vstack((Ws, W.reshape(1, L, L)))
-        print('Graph {:n} : Completed!'.format(i+1))
 
-## Visualize the Latent Space
-if visualizeLatentSpace:
-    if useJustMCW:
-        Xr = TSNE(n_components=2, perplexity=60).fit_transform(Ws.reshape(Ws.shape[0], L*80)) 
-    else:   
-        Xr = TSNE(n_components=2, perplexity=60).fit_transform(Ws.reshape(Ws.shape[0], L*L))
-    plot_latent_space(Xr, y, labels=labels)
+
