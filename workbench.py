86299df225c0542a7dba2d0ec8bfc9dc9d956ae9
Laknath1996 <ashwindesilva@yahoo.com>
2021-06-24 13:00:24 +0530

    obtaining graphs from full-event MCWs

--- workbench.py
+++ workbench.py
@@ -28,6 +28,7 @@ import os
 import numpy as np
 import scipy.io as sio
 from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
 import pandas as pd
 
 # internal
@@ -40,38 +41,124 @@ from tma.utils import plot_latent_space
 
 ## User Inputs
 id = "de_silva"
-file = "data/de_silva/subject_1001_Ashwin/R_2.csv"
-onsets_file = "data/de_silva/subject_1001_Ashwin/onsets.csv"
+data_path = "data/de_silva/subject_1002_Malsha"
+label = "PO_1"
+title = "Pointer"
+onsets_file = "data/de_silva/subject_1002_Malsha/onsets.csv"
 fs = 200
-path = "figures/subject_1001"
+path = "figures/subject_1002"
 
-data = read_from_csv(id, file)
-data = preprocess(data, fs, lpfreq=1)
-# plot_recording_same_axis(data, fs)
-
-df = pd.read_csv(onsets_file, delimiter=',')
-onsets = df[['R_2']].values.squeeze()
-
-p = 5
+## Graph Learn Params
+p = 2
 beta_1 = 10
-beta_2 = 0.1
+beta_2 = 5
 
-for i in range(len(onsets)):
-    start = int((onsets[i])*200)
-    end = int((onsets[i]+1)*200)
-    X = data[:, start:end]
+## Flags
+getOnsets = False
+plotGraphs = False
+reduceDims = True
+combinedReduceDims = False
+
+# //////////////////////
+
+if getOnsets or plotGraphs:
+    file = os.path.join(data_path, "{}.csv".format(label))
+    data = read_from_csv(id, file)
+    data = preprocess(data, fs, lpfreq=1)
     
-    GL = DiffusionGraphLearn(
-            X,
-            p=p, 
-            beta_1=beta_1,
-            beta_2=beta_2,
-            verbosity=True
-    )
-    W = GL.findGraphLaplacian()
-    save_path = os.path.join(path, "R_1_{}.png".format(i))
-    plotMCWandGraph(X, fs, W, title="Ring Flexion", save_path=save_path)
+if getOnsets:
+    plot_recording_same_axis(data, fs)
 
+if plotGraphs:
+    df = pd.read_csv(onsets_file, delimiter=',')
+    onsets = df[[label]].values.squeeze()
 
+    for i in range(len(onsets)):
+        start = int((onsets[i])*200)
+        end = int((onsets[i]+1.5)*200)
+        X = data[:, start:end]
+        
+        GL = DiffusionGraphLearn(
+                X,
+                p=p, 
+                beta_1=beta_1,
+                beta_2=beta_2,
+                verbosity=True
+        )
+        W = GL.findGraphLaplacian()
+        W /= np.max(W)
+        # W[W<0.4] = 0
+        save_path = os.path.join(path, "{}_{}.png".format(label, i))
+        plotMCWandGraph(X, fs, W, title=title, save_path=save_path)
 
+if reduceDims:
+    df = pd.read_csv(onsets_file, delimiter=',')
+    Ws = np.empty((0, 8, 8))
+    y = []
+    labels = ["M_1", "R_1", "HC_1", "V_1", "PO_1"]
+    for i in range(len(labels)):
+        file = os.path.join(data_path, "{}.csv".format(labels[i]))
+        data = read_from_csv(id, file)
+        data = preprocess(data, fs, lpfreq=1)
+        onsets = df[[labels[i]]].values.squeeze()
+        for j in range(len(onsets)):
+            print("Class {} - Graph {}".format(i, j))
+            start = int((onsets[j])*200)
+            end = int((onsets[j]+1.5)*200)
+            X = data[:, start:end]
+            
+            GL = DiffusionGraphLearn(
+                    X,
+                    p=p, 
+                    beta_1=beta_1,
+                    beta_2=beta_2,
+                    verbosity=True
+            )
+            W = GL.findGraphLaplacian()
+            W /= np.max(W)
+            # W[W<0.4] = 0
+            Ws = np.vstack((Ws, np.expand_dims(W, 0)))
+            y.append(i)
 
+    Xr = PCA(n_components=2).fit_transform(Ws.reshape(Ws.shape[0], 8*8))
+    plot_latent_space(Xr, y, labels=labels)
+
+if combinedReduceDims:
+    df = pd.read_csv(onsets_file, delimiter=',')
+    Ws = np.empty((0, 8, 8))
+    y = []
+    labels = ["M", "R", "HC", "V", "PO"]
+    for i in range(len(labels)):
+        file1 = os.path.join(data_path, "{}_2.csv".format(labels[i]))
+        file2 = os.path.join(data_path, "{}_3.csv".format(labels[i]))
+        data1 = read_from_csv(id, file1)
+        data1 = preprocess(data1, fs, lpfreq=1)
+        data2 = read_from_csv(id, file2)
+        data2 = preprocess(data2, fs, lpfreq=1)
+        onsets1 = df[['{}_2'.format(labels[i])]].values.squeeze()
+        onsets2 = df[['{}_3'.format(labels[i])]].values.squeeze()
+        onsets = np.concatenate((onsets1, onsets2))
+        for j in range(len(onsets)):
+            print("Class {} - Graph {}".format(i, j))
+            start = int((onsets[j])*200)
+            end = int((onsets[j]+1.5)*200)
+            if j < 10:
+                X = data1[:, start:end]
+            else:
+                X = data2[:, start:end]
+            
+            GL = DiffusionGraphLearn(
+                    X,
+                    p=p, 
+                    beta_1=beta_1,
+                    beta_2=beta_2,
+                    verbosity=True
+            )
+            W = GL.findGraphLaplacian()
+            W /= np.max(W)
+            W[W<0.4] = 0
+            Ws = np.vstack((Ws, np.expand_dims(W, 0)))
+            y.append(i)
+
+    Xr = PCA(n_components=2).fit_transform(Ws.reshape(Ws.shape[0], 8*8))
+    plot_latent_space(Xr, y, labels=labels)
